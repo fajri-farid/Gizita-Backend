d56e5caf9a433adace1f932ea3a4381fd36f97dd
fajri-farid <fajrijry14@gmail.com>
2024-11-21 15:24:02 +0800

    add user table

---
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
