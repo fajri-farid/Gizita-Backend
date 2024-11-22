6f8cf1b36b4e0c875e3fb8605b7f48031daa9021
fajri-farid <fajrijry14@gmail.com>
2024-11-22 20:24:05 +0800

    feat: make more better for register and structure folder

--- demo/src/main/java/com/example/demo/Model/RegisterUserDTO.java
+++ demo/src/main/java/com/example/demo/Model/RegisterUserDTO.java
@@ -9,28 +9,4 @@ public class RegisterUserDTO {
     private String email;
     private String userName;
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
