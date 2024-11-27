a1903c854d4ebb70b54e72fcc476b7564002c773
fajri-farid <fajrijry14@gmail.com>
2024-11-27 10:51:12 +0800

    fix: delete something that doesn't need anymore

--- demo/src/main/java/com/example/demo/Repository/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student,Integer> {
-
-}
